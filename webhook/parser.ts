7ee4235a5972638671baa578ecbbeef13356eade
tatsuya <t2h5k0@gmail.com>
2020-02-04 22:55:57 +0900

    update parser
    pull_request: review_requested

--- webhook/parser.ts
+++ webhook/parser.ts
@@ -5,6 +5,39 @@ export const parseEvent = (
   event: APIGatewayProxyEvent,
 ): IncomingWebhookSendArguments | null => {
   const eventJson = JSON.parse(event.body || '')
+  // pull_request: review_requested
+  if (eventJson.action === 'review_requested') {
+    const repository = eventJson.repository
+    const pullRequest = eventJson.pull_request
+    const reviewers = pullRequest.requested_reviewers
+      .map(reviewer => {
+        return reviewer.login
+      })
+      .join(', ')
+    const color = 'good'
+    const text = 'review requested'
+    const fields = [
+      {
+        title: pullRequest.title,
+        value: pullRequest.html_url,
+      },
+      {
+        title: 'reviewers',
+        value: reviewers,
+      },
+    ]
+    return {
+      attachments: [
+        {
+          color,
+          title: repository.name,
+          title_link: repository.html_url, // eslint-disable-line @typescript-eslint/camelcase
+          text,
+          fields,
+        },
+      ],
+    }
+  }
   // pull_request_review
   if (eventJson.review) {
     const repository = eventJson.repository
